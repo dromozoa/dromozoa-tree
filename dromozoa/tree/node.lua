df2cb53a829ea7db8684f29fa2958faaf2252d45
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-11-11 15:28:01 +0900

    duplicateを追加。

--- dromozoa/tree/node.lua
+++ dromozoa/tree/node.lua
@@ -15,6 +15,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-tree.  If not, see <http://www.gnu.org/licenses/>.
 
+local clone = require "dromozoa.commons.clone"
 local sequence = require "dromozoa.commons.sequence"
 local bfs = require "dromozoa.tree.bfs"
 local dfs = require "dromozoa.tree.dfs"
@@ -150,6 +151,24 @@ end
 function class:dfs(visitor)
   local uid, model, props, tree = unpack_item(self)
   dfs(tree, visitor, self)
+end
+
+function class:duplicate()
+  local uid, model, props, tree = unpack_item(self)
+  local map = {}
+  self:dfs({
+    discover_node = function (_, a)
+      local b = tree:create_node()
+      map[a.id] = b.id
+      for k, v in a:each_property() do
+        b[clone(k)] = clone(v)
+      end
+    end;
+    finish_edge = function (_, a, b)
+      model:append_child(map[a.id], map[b.id])
+    end;
+  })
+  return tree:get_node(map[uid]), map
 end
 
 function class:collapse()
---
+++ test/test_duplicate.lua
@@ -0,0 +1,45 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-tree.
+--
+-- dromozoa-tree is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-tree is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-tree.  If not, see <http://www.gnu.org/licenses/>.
+
+local tree = require "dromozoa.tree"
+
+local t = tree()
+
+local n1 = t:create_node()
+local n2 = t:create_node()
+local n3 = t:create_node()
+local n4 = t:create_node()
+
+n3.color = 1
+n4.color = 2
+
+n1:append_child(n2)
+n2:append_child(n3)
+n2:append_child(n4)
+
+local n5, map = n2:duplicate()
+
+n1:append_child(n5)
+
+local children = n5:children()
+assert(#children == 2)
+local n6 = children[1]
+local n7 = children[2]
+assert(map[n3.id] == n6.id)
+assert(map[n4.id] == n7.id)
+assert(n6.color == 1)
+assert(n7.color == 2)
