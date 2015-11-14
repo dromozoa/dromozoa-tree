9451552894da59c8515eb8ef2bb567f870944731
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-11-14 16:20:17 +0900

    インターフェースを拡張。

---
+++ test/test_delete.lua
@@ -0,0 +1,36 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-tree.
+--
+-- dromozoa-tree is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-tree is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-tree.  If not, see <http://www.gnu.org/licenses/>.
+
+local tree = require "dromozoa.tree"
+
+local t = tree()
+local n1 = t:create_node()
+local n2 = t:create_node()
+local n3 = t:create_node()
+local n4 = t:create_node()
+local n5 = t:create_node()
+
+n1:append_child(n2)
+n1:append_child(n3)
+n2:append_child(n4)
+n2:append_child(n5)
+
+assert(t:count_node() == 5)
+n2:remove()
+assert(t:count_node() == 5)
+n2:delete(true)
+assert(t:count_node() == 2)
